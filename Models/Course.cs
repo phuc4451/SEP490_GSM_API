669b034b59963bb6811ceb752c20c598d3539722
phuc4451 <105834705+phuc4451@users.noreply.github.com>
2024-10-23 02:22:33 +0700

    Update Course.cs
    courseDuration changed int to string

--- Models/Course.cs
+++ Models/Course.cs
@@ -5,7 +5,7 @@
 		public string CourseId { get; set; }
 		public string CourseName { get; set; }
 		public string CourseContent { get; set; }
-		public int CourseDuration { get; set; }
+		public string CourseDuration { get; set; }
 		//public string CourseFeedback { get; set; }
 		public int CoursePrice { get; set; }
 	}
