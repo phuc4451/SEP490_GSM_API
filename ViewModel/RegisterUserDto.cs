c5ef8678cf1d079f1603079481b5fa3305c73307
phuc4451 <105834705+phuc4451@users.noreply.github.com>
2024-11-15 01:07:07 +0700

    register not require phone

--- ViewModel/RegisterUserDto.cs
+++ ViewModel/RegisterUserDto.cs
@@ -5,7 +5,7 @@
 		public string Email { get; set; }
 
 		public string Password { get; set; }
-		public string Phone { get; set; }
+		public string? Phone { get; set; }
 
 		public string Name { get; set; }
 	}
