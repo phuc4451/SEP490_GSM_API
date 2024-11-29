e94d505d7b2b194a4d776b63eb5130d5c42aa22f
phuc4451 <105834705+phuc4451@users.noreply.github.com>
2024-11-29 19:52:49 +0700

    optimize query customer accounts

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -123,17 +123,28 @@ public class UsersController : ControllerBase
 	public async Task<ActionResult<IEnumerable<User>>> GetCustomers()
 	{
 		_firebaseClient = _firebaseClientProvider.GetFirebaseClient();
+
+		var roles = await _roleService.GetAllRoles();
+
+		var customerRoleId = roles.FirstOrDefault(role => role.RoleName == "customer")?.RoleId;
+
 		var users = await _firebaseClient
 			.Child("users")
+			.OrderBy("roleId")
+			.EqualTo(customerRoleId)
 			.OnceAsync<User>();
 
-		var userList = new List<User>();
-		foreach (var user in users)
-		{
-			var roleName = await _roleService.GetRoleName(user.Object.RoleId);
-			if (roleName == "customer")
-				userList.Add(user.Object);
-		}
+		var userList = users
+			.Select(user => user.Object)
+			.ToList();
+
+		//var userList = new List<User>();
+		//foreach (var user in users)
+		//{
+		//	var roleName = await _roleService.GetRoleName(user.Object.RoleId);
+		//	if (roleName == "customer")
+		//		userList.Add(user.Object);
+		//}
 
 		return Ok(userList);
 	}
