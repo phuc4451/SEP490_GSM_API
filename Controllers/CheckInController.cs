ec0ec51cfe81873fdda7908684506507c5e66e8d
OCEAN <dinhduong1492003@gmail.com>
2024-12-17 09:19:07 +0700

    Update CheckIn Newest Code - DuongDDHE176496
    Update CheckIn Newest Code - DuongDDHE176496

--- Controllers/CheckInController.cs
+++ Controllers/CheckInController.cs
@@ -60,174 +60,174 @@ namespace Alpha_API.Controllers
 			};
 
             #region
-            //if (role.Equals("customer"))
-            //{
+            if (role.Equals("customer"))
+            {
 
-            //	var boxReg = _firebaseClient.Child("BoxingRegistrations").OrderBy("isActive").EqualTo(true).OnceAsync<BoxingRegistration>();
+                var boxReg = _firebaseClient.Child("BoxingRegistrations").OrderBy("isActive").EqualTo(true).OnceAsync<BoxingRegistration>();
 
-            //	var gymReg = _firebaseClient.Child("GymRegistrations").OrderBy("userId").EqualTo(request.UserId).OnceAsync<GymRegistration>();
+                var gymReg = _firebaseClient.Child("GymRegistrations").OrderBy("userId").EqualTo(request.UserId).OnceAsync<GymRegistration>();
 
-            //	var rentalReg = _firebaseClient.Child("TrainerRentalRegistrations").OrderBy("isActive").EqualTo(true).OnceAsync<TrainerRentalRegistration>();
+                var rentalReg = _firebaseClient.Child("TrainerRentalRegistrations").OrderBy("isActive").EqualTo(true).OnceAsync<TrainerRentalRegistration>();
 
-            //	await Task.WhenAll(boxReg, gymReg);
+                await Task.WhenAll(boxReg, gymReg);
 
-            //	var gymMembership = gymReg.Result.FirstOrDefault(exReg =>
-            //		exReg.Object.IsActive &&
-            //		(exReg.Object.EndDate >= DateTime.Now || exReg.Object.SessionLeft > 0));
+                var gymMembership = gymReg.Result.FirstOrDefault(exReg =>
+                    exReg.Object.IsActive &&
+                    (exReg.Object.EndDate >= DateTime.Now || exReg.Object.SessionLeft > 0));
 
-            //	var boxingMembership = boxReg.Result.FirstOrDefault(reg =>
-            //		reg.Object.UserIds != null &&
-            //		reg.Object.UserIds.Split(',').Any(userId => userId.Equals(request.UserId)) &&
-            //		(reg.Object.SessionLeft > 0 && reg.Object.EndDate >= DateTime.Now));
+                var boxingMembership = boxReg.Result.FirstOrDefault(reg =>
+                    reg.Object.UserIds != null &&
+                    reg.Object.UserIds.Split(',').Any(userId => userId.Equals(request.UserId)) &&
+                    (reg.Object.SessionLeft > 0 && reg.Object.EndDate >= DateTime.Now));
 
-            //	var rentalMembership = rentalReg.Result.FirstOrDefault(reg =>
-            //			reg.Object.UserIds != null &&
-            //			reg.Object.UserIds.Split(',').Any(userId => userId.Equals(request.UserId)) &&
-            //			(reg.Object.EndDate >= DateTime.Now || reg.Object.SessionLeft > 0));
+                var rentalMembership = rentalReg.Result.FirstOrDefault(reg =>
+                        reg.Object.UserIds != null &&
+                        reg.Object.UserIds.Split(',').Any(userId => userId.Equals(request.UserId)) &&
+                        (reg.Object.EndDate >= DateTime.Now || reg.Object.SessionLeft > 0));
 
-            //	var hasGymMembership = gymMembership != null;
-            //	var hasBoxingMembership = boxingMembership != null;
-            //	var hasTrainerRentalMembership = rentalMembership != null;
+                var hasGymMembership = gymMembership != null;
+                var hasBoxingMembership = boxingMembership != null;
+                var hasTrainerRentalMembership = rentalMembership != null;
 
-            //	if (!hasGymMembership && !hasBoxingMembership)
-            //	{
-            //		return NotFound("User do not have any memberships");
-            //	}
+                if (!hasGymMembership && !hasBoxingMembership)
+                {
+                    return NotFound("User do not have any memberships");
+                }
 
-            //	if (hasGymMembership && !hasTrainerRentalMembership && !hasBoxingMembership)
-            //	{
-            //		var checkInTask = _firebaseClient.Child("CheckIns").PostAsync(newCheckIn);
-            //		var gymTask = _firebaseClient.Child("GymRegistrations").Child(gymMembership.Object.RegistrationId).PatchAsync(new
-            //		{
-            //			sessionLeft = gymMembership.Object.SessionLeft - 1
-            //		});
+                if (hasGymMembership && !hasTrainerRentalMembership && !hasBoxingMembership)
+                {
+                    var checkInTask = _firebaseClient.Child("CheckIns").PostAsync(newCheckIn);
+                    var gymTask = _firebaseClient.Child("GymRegistrations").Child(gymMembership.Object.RegistrationId).PatchAsync(new
+                    {
+                        sessionLeft = gymMembership.Object.SessionLeft - 1
+                    });
 
-            //		await Task.WhenAll(checkInTask, gymTask);
-            //		return Ok("Check-in recorded successfully.");
-            //	}
+                    await Task.WhenAll(checkInTask, gymTask);
+                    return Ok("Check-in recorded successfully.");
+                }
 
-            //	// Cache current time details
-            //	var currentDate = DateOnly.FromDateTime(DateTime.Now);
-            //	var currentTime = TimeOnly.FromDateTime(DateTime.Now);
+                // Cache current time details
+                var currentDate = DateOnly.FromDateTime(DateTime.Now);
+                var currentTime = TimeOnly.FromDateTime(DateTime.Now);
 
-            //	// Query schedules for the user within the current date range
-            //	var schedules = (await _firebaseClient
-            //		.Child("Schedules")
-            //		.OnceAsync<Schedule>())
-            //		.Where(schedule =>
-            //			schedule.Object.UserIds.Split(',')
-            //				.Any(userId => userId == request.UserId) &&
-            //			schedule.Object.FirstSlot <= currentDate &&
-            //			schedule.Object.LastSlot >= currentDate)
-            //		.ToList();
-            //	if (!schedules.Any())
-            //		return NotFound("No schedules found for the user.");
+                // Query schedules for the user within the current date range
+                var schedules = (await _firebaseClient
+                    .Child("Schedules")
+                    .OnceAsync<Schedule>())
+                    .Where(schedule =>
+                        schedule.Object.UserIds.Split(',')
+                            .Any(userId => userId == request.UserId) &&
+                        schedule.Object.FirstSlot <= currentDate &&
+                        schedule.Object.LastSlot >= currentDate)
+                    .ToList();
+                if (!schedules.Any())
+                    return NotFound("No schedules found for the user.");
 
-            //	// Pre-fetch all slots related to the schedules
-            //	var scheduleKeys = schedules.Select(s => s.Key).ToHashSet();
-            //	var slots = (await _firebaseClient
-            //		.Child("Slots")
-            //		.OnceAsync<Slot>())
-            //		.Where(slot => scheduleKeys.Contains(slot.Object.ScheduleId) && slot.Object.Date == currentDate)
-            //		.ToList();
+                // Pre-fetch all slots related to the schedules
+                var scheduleKeys = schedules.Select(s => s.Key).ToHashSet();
+                var slots = (await _firebaseClient
+                    .Child("Slots")
+                    .OnceAsync<Slot>())
+                    .Where(slot => scheduleKeys.Contains(slot.Object.ScheduleId) && slot.Object.Date == currentDate)
+                    .ToList();
 
-            //	if (!slots.Any())
-            //	{
-            //		if (hasBoxingMembership && !hasTrainerRentalMembership && !hasGymMembership)
-            //		{
-            //			return Conflict("No slots found for today.");
-            //		}
-            //		else
-            //		{
-            //			var checkInTask = _firebaseClient.Child("CheckIns").PostAsync(newCheckIn);
-            //			var gymTask = _firebaseClient.Child("GymRegistrations").Child(gymMembership.Object.RegistrationId).PatchAsync(new
-            //			{
-            //				sessionLeft = gymMembership.Object.SessionLeft - 1
-            //			});
+                if (!slots.Any())
+                {
+                    if (hasBoxingMembership && !hasTrainerRentalMembership && !hasGymMembership)
+                    {
+                        return Conflict("No slots found for today.");
+                    }
+                    else
+                    {
+                        var checkInTask = _firebaseClient.Child("CheckIns").PostAsync(newCheckIn);
+                        var gymTask = _firebaseClient.Child("GymRegistrations").Child(gymMembership.Object.RegistrationId).PatchAsync(new
+                        {
+                            sessionLeft = gymMembership.Object.SessionLeft - 1
+                        });
 
-            //			await Task.WhenAll(checkInTask, gymTask);
-            //			return Ok("Check-in recorded successfully.");
-            //		}
-            //	}
-            //	else
-            //	{
-            //		// Process slots and update attendance
-            //		foreach (var slot in slots)
-            //		{
-            //			var timeRange = _timeSlotService.GetTimeSlot(slot.Object.TimeSlotId).Split('-');
-            //			if (timeRange.Length == 2 &&
-            //				//customer comes 30 minutes early
-            //				TimeOnly.ParseExact(timeRange[0], "H:mm").AddMinutes(-30) <= currentTime &&
-            //				TimeOnly.ParseExact(timeRange[1], "H:mm") >= currentTime)
-            //			{
-            //				var slotTask = _firebaseClient.Child("Slots").Child(slot.Key).PatchAsync(new
-            //				{
-            //					attended = true
-            //				});
+                        await Task.WhenAll(checkInTask, gymTask);
+                        return Ok("Check-in recorded successfully.");
+                    }
+                }
+                else
+                {
+                    // Process slots and update attendance
+                    foreach (var slot in slots)
+                    {
+                        var timeRange = _timeSlotService.GetTimeSlot(slot.Object.TimeSlotId).Split('-');
+                        if (timeRange.Length == 2 &&
+                            //customer comes 30 minutes early
+                            TimeOnly.ParseExact(timeRange[0], "H:mm").AddMinutes(-30) <= currentTime &&
+                            TimeOnly.ParseExact(timeRange[1], "H:mm") >= currentTime)
+                        {
+                            var slotTask = _firebaseClient.Child("Slots").Child(slot.Key).PatchAsync(new
+                            {
+                                attended = true
+                            });
 
-            //				var boxingTask = _firebaseClient.Child("BoxingRegistrations").Child(boxingMembership.Object.RegistrationId).PatchAsync(new
-            //				{
-            //					sessionLeft = boxingMembership.Object.SessionLeft - 1
-            //				});
+                            var boxingTask = _firebaseClient.Child("BoxingRegistrations").Child(boxingMembership.Object.RegistrationId).PatchAsync(new
+                            {
+                                sessionLeft = boxingMembership.Object.SessionLeft - 1
+                            });
 
-            //				var gymTask = _firebaseClient.Child("GymRegistrations").Child(gymMembership.Object.RegistrationId).PatchAsync(new
-            //				{
-            //					sessionLeft = gymMembership.Object.SessionLeft - 1
-            //				});
+                            var gymTask = _firebaseClient.Child("GymRegistrations").Child(gymMembership.Object.RegistrationId).PatchAsync(new
+                            {
+                                sessionLeft = gymMembership.Object.SessionLeft - 1
+                            });
 
-            //				var rentalTask = _firebaseClient.Child("TrainerRentalRegistrations").Child(rentalMembership.Object.RegistrationId).PatchAsync(new
-            //				{
-            //					sessionLeft = rentalMembership.Object.SessionLeft - 1
-            //				});
+                            var rentalTask = _firebaseClient.Child("TrainerRentalRegistrations").Child(rentalMembership.Object.RegistrationId).PatchAsync(new
+                            {
+                                sessionLeft = rentalMembership.Object.SessionLeft - 1
+                            });
 
-            //				if (hasBoxingMembership && !hasGymMembership)
-            //				{
-            //					await Task.WhenAll(slotTask, boxingTask);
-            //				}
-            //				else if (hasBoxingMembership && hasGymMembership && !hasTrainerRentalMembership)
-            //				{
-            //					await Task.WhenAll(slotTask, boxingTask, gymTask);
-            //				}
-            //				else if (!hasBoxingMembership && hasGymMembership && hasTrainerRentalMembership)
-            //				{
-            //					await Task.WhenAll(slotTask, gymTask, rentalTask);
-            //				}
-            //				else if (hasBoxingMembership && hasGymMembership && hasTrainerRentalMembership)
-            //				{
-            //					if (slot.Object.ScheduleId == boxingMembership.Object.ScheduleId)
-            //						await Task.WhenAll(slotTask, boxingTask, gymTask);
-            //					else
-            //						await Task.WhenAll(slotTask, gymTask, rentalTask);
-            //				}
-            //				return Ok($"Check-in recorded successfully. Slot: {_timeSlotService.GetTimeSlot(slot.Object.TimeSlotId)}");
-            //			}
-            //		}
+                            if (hasBoxingMembership && !hasGymMembership)
+                            {
+                                await Task.WhenAll(slotTask, boxingTask);
+                            }
+                            else if (hasBoxingMembership && hasGymMembership && !hasTrainerRentalMembership)
+                            {
+                                await Task.WhenAll(slotTask, boxingTask, gymTask);
+                            }
+                            else if (!hasBoxingMembership && hasGymMembership && hasTrainerRentalMembership)
+                            {
+                                await Task.WhenAll(slotTask, gymTask, rentalTask);
+                            }
+                            else if (hasBoxingMembership && hasGymMembership && hasTrainerRentalMembership)
+                            {
+                                if (slot.Object.ScheduleId == boxingMembership.Object.ScheduleId)
+                                    await Task.WhenAll(slotTask, boxingTask, gymTask);
+                                else
+                                    await Task.WhenAll(slotTask, gymTask, rentalTask);
+                            }
+                            return Ok($"Check-in recorded successfully. Slot: {_timeSlotService.GetTimeSlot(slot.Object.TimeSlotId)}");
+                        }
+                    }
 
-            //		//if (currentTime > slots.Max(x => TimeOnly.ParseExact(_timeSlotService.GetTimeSlot(x.Object.TimeSlotId).Split('-')[0], "H:mm")))
-            //		//{
-            //		//	return Ok("Check-in recorded successfully.");
-            //		//}
+                    //if (currentTime > slots.Max(x => TimeOnly.ParseExact(_timeSlotService.GetTimeSlot(x.Object.TimeSlotId).Split('-')[0], "H:mm")))
+                    //{
+                    //	return Ok("Check-in recorded successfully.");
+                    //}
 
-            //		//too late or too early
-            //		if (hasBoxingMembership && !hasTrainerRentalMembership && !hasGymMembership)
-            //			return Conflict("Check-in failed. No slot available now");
-            //		else
-            //		{
-            //			var checkInTask = _firebaseClient.Child("CheckIns").PostAsync(newCheckIn);
-            //			var gymTask = _firebaseClient.Child("GymRegistrations").Child(gymMembership.Object.RegistrationId).PatchAsync(new
-            //			{
-            //				sessionLeft = gymMembership.Object.SessionLeft - 1
-            //			});
+                    //too late or too early
+                    if (hasBoxingMembership && !hasTrainerRentalMembership && !hasGymMembership)
+                        return Conflict("Check-in failed. No slot available now");
+                    else
+                    {
+                        var checkInTask = _firebaseClient.Child("CheckIns").PostAsync(newCheckIn);
+                        var gymTask = _firebaseClient.Child("GymRegistrations").Child(gymMembership.Object.RegistrationId).PatchAsync(new
+                        {
+                            sessionLeft = gymMembership.Object.SessionLeft - 1
+                        });
 
-            //			await Task.WhenAll(checkInTask, gymTask);
-            //			return Ok("Check-in recorded successfully.");
-            //		}
-            //	}
+                        await Task.WhenAll(checkInTask, gymTask);
+                        return Ok("Check-in recorded successfully.");
+                    }
+                }
 
-            //}
+            }
             #endregion old
 
-            if (role.Equals("customer"))
+            /*if (role.Equals("customer"))
             {
                 // Query for Gym registration based on userId
                 var gymReg = _firebaseClient.Child("GymRegistrations").OrderBy("userId").EqualTo(request.UserId).OnceAsync<GymRegistration>();
@@ -253,7 +253,7 @@ namespace Alpha_API.Controllers
                 await Task.WhenAll(checkInTask, gymTask);
 
                 return Ok("Check-in recorded successfully.");
-            }
+            }*/
 
             else if (role.Equals("staff"))
 			{
